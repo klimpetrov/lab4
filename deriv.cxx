59d7b39fcaa6310cb48ab22744ca2df4c76961b1
Klim Petrov <klpetr@pc01.pool.tp1.uni-duesseldorf.de>
2015-11-11 17:52:26 +0100

    kjf

--- deriv.cxx
+++ deriv.cxx
@@ -1,18 +1,41 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 
 using namespace std;
 
 
 void print(double* p, const int N, const double dx,
-           const double xmin)
-{
-   for(int i=0; i<N; i++)
+           const double xmin){
+   ofstream out("output.txt");
+   for(int i=0; i<N; i++){
+     
+       out << i*dx + xmin << "\t" << p[i] << endl;
+      
        cout << i*dx + xmin << "\t" << p[i] << endl;
+    }
+out.close();
+}
+void rein(double* p, const double xmin, const int N, const double dx){
+    for(int i=0; i<N; i++){
+	p[i] = exp(-pow(xmin+i*dx,2));
+    }
 }
 
+void abl(double* p, const int N, const double dx){
+    double a,b;
+    p[0]=0;
+    p[N-1] =0;
+    b = 0;
+  for(int i=1; i<N-1; i++){
+    a=p[i];
+   p[i] = (p[i+1]-b)/(2*dx);
+    b=a;
+  }
+}
 
 int main(){
+ 
   const int N = 256;
   double p[N];
   const double xmax = 15;
@@ -20,8 +43,12 @@ int main(){
   const double dx = (xmax-xmin)/(N-1);
 
   // call to function which fills array p here
+  rein(p, xmin,N, dx);
+  
   // call to functio which calculates the derivative
+  abl(p, N, dx);
   print(p,N,dx,xmin);
+ 
 
   return 0;
 }
